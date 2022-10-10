f46ef2f6a22f49cb8638dc9a9c8c10d85b252593
izabellaalves <iap21010705@gmail.com>
2022-10-10 18:09:39 -0300

    .

---
+++ Analisador de números/script.js
@@ -0,0 +1,47 @@
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >=1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML=''
+    }else {
+        window.alert('Valor inválido ou já está na lista.')
+    }
+    num.value= ""
+    num.focus()
+}
+
+function finalizar () {
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+
+        
+    }
+}
